05af979863f3c218abf68d9d70d086d1bb18fe37
Eugene-TechnoNinja <technoninja@ukr.net>
2025-01-27 17:24:46 +0200

    reliable navigation of flying platforms done

--- src/modules/platform.js
+++ src/modules/platform.js
@@ -26,12 +26,12 @@ export const Platform = (function implementer() {
                 start ??= this.pos;
                 if (type === 'vertical') {
                     this.move(0, dir * this.speed);
-                    if (Math.round(this.pos.y) === Math.round(endY)) dir = 1;
-                    if (Math.round(this.pos.y) === Math.round(start.y)) dir = -1;
+                    if (Math.round(this.pos.y) <= Math.round(endY)) dir = 1;
+                    if (Math.round(this.pos.y) >= Math.round(start.y)) dir = -1;
                 } else if (type === 'horizontal') {
                     this.move(dir * this.speed, 0);
-                    if (Math.round(this.pos.x) === Math.round(endX)) dir = -1;
-                    if (Math.round(this.pos.x) === Math.round(start.x)) dir = 1;
+                    if (Math.round(this.pos.x) >= Math.round(endX)) dir = -1;
+                    if (Math.round(this.pos.x) <= Math.round(start.x)) dir = 1;
                 }
             },
         };
